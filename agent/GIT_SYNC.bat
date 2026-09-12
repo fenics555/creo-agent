d55128e65c3d93bbb4964487c1042a82a0f8a730
Sergio <sergio@frezer4>
2026-09-12 23:33:02 +0300

    autosave 12.09.2026 23:33:02,87

--- agent/GIT_SYNC.bat
+++ agent/GIT_SYNC.bat
@@ -2,10 +2,10 @@
 set "ROOT=D:\AI\tools"
 set "SLOG=%ROOT%\git_sync.log"
 cd /d "%ROOT%"
-git add -A && (
-    git commit -m "autosave %date% %time%" >> "%SLOG%" 2>&1 && (
-        git push origin master >> "%SLOG%" 2>&1
-    )
-)
+git add -A
+if %errorlevel% neq 0 exit /b %errorlevel%
+git commit -m "autosave %date% %time%" >> "%SLOG%" 2>&1
+if %errorlevel% neq 0 exit /b %errorlevel%
+git push origin master >> "%SLOG%" 2>&1
 exit /b %errorlevel%
 
